77fec57fb1b0a6e732738644eecd2306e16f490b
RODRIGO MANOZZO <rodrigomanozzo@gmail.com>
2021-02-24 01:03:51 -0300

    complete tasks list

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,8 @@ export default function Home() {
     setInput({ ...input, [e.target.name]: e.target.value });
   };
 
-  function handleAddTodo() {
+  function handleAddTodo(event) {
+    event.preventDefault();
     setTodos([...todos, { id: Math.random(), title: input.todoInput }]);
     setInput({ todoInput: '' });
   }
@@ -30,7 +31,10 @@ export default function Home() {
 
   useEffect(() => {
     const filtered = todos.filter(todo => todo.complete);
-    document.title = `Você tem ${filtered.length} to do completas`
+    if (filtered.length <= 1)
+      document.title = `Você tem ${filtered.length} tarefa completa.`
+    else
+      document.title = `Você tem ${filtered.length} tarefas completas.`
   }, [todos]);
 
   return (
@@ -41,27 +45,44 @@ export default function Home() {
         </h1>
 
         <div className={styles.card}>
-          <input value={input.todoInput} name={'todoInput'} className={styles.inputAdd} onChange={(event) => handleChangeInputTodo(event)} />
-          <button className={styles.buttonAdd} onClick={() => handleAddTodo()} placeholder="add todos here...">+</button>
+          <form><input value={input.todoInput} name={'todoInput'} className={styles.inputAdd} onChange={(event) => handleChangeInputTodo(event)} />
+          <button className={styles.buttonAdd} onClick={(event) => handleAddTodo(event)} placeholder="add todos here...">+</button></form>
           
-          <p className={styles.info}> Your Todos: </p>
+          <p className={styles.info}> Your Tasks: </p>
           <>
             {todos.map((todo) => {
               return (
                 <ul className={styles.todoList}>
                   <li key={todo.id}>
                     <label style={{ textDecoration: todo.complete === true ? "line-through" : "" }} className={styles.labelTodo}><input type="checkbox" onChange={() => handleCompleteTodo(todo.id)}/> {todo.title}</label>
-                    <span onClick={() => handleRemoveTodo(todo.id)}><FontAwesomeIcon className={styles.icon} icon={faTrash} /></span>
+                    <span className={styles.icon} onClick={() => handleRemoveTodo(todo.id)}>X</span>
                   </li>
                 </ul>
               )
             })
             }
           </>
+
+          <p className={styles.info}> Your Complete Tasks: </p>
+          <>
+            {todos.map((todo) => {
+              if (todo.complete)
+                return (
+                  <ul className={styles.todoList}>
+                    <li key={todo.id}>
+                      <label className={styles.labelTodo}> {todo.title}</label>
+                      <span onClick={() => handleRemoveTodo(todo.id)}><FontAwesomeIcon className={styles.icon} icon={faTrash} /></span>
+                    </li>
+                  </ul>
+              )
+            })
+            }
+          </>
+
         </div>
       </main>
       <footer className={styles.footer}>
-        Powered by Rodrigo Manozzo e Gabriel Palioqui
+        Powered by <a className={styles.code} href="https://www.linkedin.com/in/rodrigo-manozzo-715a8273/" target="_blank"> Rodrigo Manozzo </a> e <a className={styles.code} href="https://www.linkedin.com/in/gabeluiz" target="_blank"> Gabriel Palioqui </a>
       </footer>
     </div >
   )
