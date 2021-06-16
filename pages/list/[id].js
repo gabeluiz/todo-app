d9394a3254825d7824d7d68afbc639c05d02d762
Gabriel Luiz <gabeluiz10@gmail.com>
2021-06-15 22:55:05 -0300

    add new component list

--- pages/list/[id].js
+++ pages/list/[id].js
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/router';
 import Layout from '../../components/layout';
 import Toolbar from '../../components/toolbar';
-import ListTask from '../../components/list-item';
+import ListTask from '../../components/list-task';
 
 export default function List() {
 
