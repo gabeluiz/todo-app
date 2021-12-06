915f31c0269ad0abbee49e60f9ecfbb56ec3fdca
Gabriel Luiz <gabeluiz10@gmail.com>
2021-12-06 20:39:44 -0300

    new feature, toggle webcam

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,29 @@
-import React from 'react';
-import Layout from '../components/layout';
-import Toolbar from '../components/toolbar';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from "react";
+import Layout from "../components/layout";
+import Toolbar from "../components/toolbar";
+import { makeStyles } from "@material-ui/core/styles";
 //User SESSION
-import { useSession } from 'next-auth/client';
-import { Typography, Grid } from '@material-ui/core';
+import { useSession } from "next-auth/client";
+import { Typography, Grid } from "@material-ui/core";
+import WebcamVideo from "../components/WebcamVideo";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-  }
+    display: "flex",
+  },
 }));
 
 function Home() {
-
   const [session, loading] = useSession();
   const classes = useStyles();
+  const [toggleTab, setToggleTab] = useState(true);
 
   return (
     <Layout>
       <Toolbar />
       {!session && (
         <>
-          <Typography variant="h5">
-            Login
-          </Typography>
+          <Typography variant="h5">Login</Typography>
           <Typography variant="subtitle2">
             You must be signed in to view this page
           </Typography>
@@ -34,15 +33,26 @@ function Home() {
         <div className={classes.root}>
           <Grid container spacing={3}>
             <Grid item xs={12}>
-              <Typography variant="h5">
-                Welcome
-              </Typography>
+              <Typography>Welcome</Typography>
+              {toggleTab ? (
+                <div>
+                  <WebcamVideo />
+                </div>
+              ) : (
+                <div>
+                  <div>Not webcam</div>
+                  <div>None</div>
+                </div>
+              )}
+              <button onClick={() => setToggleTab(!toggleTab)}>
+                Toggle webcam
+              </button>
             </Grid>
           </Grid>
         </div>
       )}
     </Layout>
-  )
+  );
 }
 
 export default Home;
